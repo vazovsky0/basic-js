b7247034c76ab61753f91991be90603655c5347c
Zaur Gurbanov <crypton.qurbanov.1991@gmail.com>
2025-11-06 02:21:45 +0400

    feat: add solution of task transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../lib');
+const { NotImplementedError } = require("../lib");
 
 /**
  * Create transformed array based on the control sequences that original
@@ -13,11 +13,54 @@ const { NotImplementedError } = require('../lib');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  *
  */
-function transform(/* arr */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function transform(arr) {
+  // 1. Проверка на некорректный аргумент
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+
+  const result = [];
+  const tempArr = [...arr];
+
+  for (let i = 0; i < tempArr.length; i++) {
+    const current = tempArr[i];
+
+    switch (current) {
+      case "--discard-next":
+        if (i < tempArr.length - 1) {
+          tempArr[i + 1] = null;
+        }
+        break;
+
+      case "--discard-prev":
+        if (i > 0 && tempArr[i - 1] !== null) {
+          result.pop();
+        }
+        break;
+
+      case "--double-next":
+        if (i < tempArr.length - 1) {
+          result.push(tempArr[i + 1]);
+        }
+        break;
+
+      case "--double-prev":
+        if (i > 0 && tempArr[i - 1] !== null) {
+          result.push(tempArr[i - 1]);
+        }
+        break;
+
+      default:
+        if (current !== null) {
+          result.push(current);
+        }
+        break;
+    }
+  }
+
+  return result;
 }
 
 module.exports = {
-  transform
+  transform,
 };
