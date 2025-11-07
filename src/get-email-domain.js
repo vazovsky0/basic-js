8a347696aa09b14af2824b8185bb0c5e148e5432
Zaur Gurbanov <crypton.qurbanov.1991@gmail.com>
2025-11-07 15:50:28 +0400

    feat: add solution of task get-email-domain

--- src/get-email-domain.js
+++ src/get-email-domain.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../lib');
+const { NotImplementedError } = require("../lib");
 
 /**
  * Given an email address, return it's domain.
@@ -10,10 +10,15 @@ const { NotImplementedError } = require('../lib');
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-function getEmailDomain(/* email */) {
-  throw new NotImplementedError('Not implemented');
+function getEmailDomain(email) {
+  if (typeof email !== "string") {
+    throw new NotImplementedError("Not implemented");
+  } else {
+    const atIndex = email.lastIndexOf("@");
+    return email.slice(atIndex + 1);
+  }
 }
 
 module.exports = {
-  getEmailDomain
+  getEmailDomain,
 };
