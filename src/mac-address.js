6767cbb2db5ce125486816fbf94d0e9e5fddd6c0
Zaur Gurbanov <crypton.qurbanov.1991@gmail.com>
2025-11-07 16:43:33 +0400

    feat: add solution of task mac-address

--- src/mac-address.js
+++ src/mac-address.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../lib');
+const { NotImplementedError } = require("../lib");
 
 /**
  * The MAC-48 address is six groups of two hexadecimal digits (0 to 9 or A to F),
@@ -14,11 +14,15 @@ const { NotImplementedError } = require('../lib');
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function isMAC48Address(inputString) {
+  // Регулярное выражение для проверки MAC-48 формата
+  const macPattern = /^([0-9A-F]{2}-){5}[0-9A-F]{2}$/;
+
+  return macPattern.test(inputString);
 }
 
+console.log(isMAC48Address("00-1B-63-84-45-E6"));
+
 module.exports = {
-  isMAC48Address
+  isMAC48Address,
 };
